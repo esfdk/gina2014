fdbe167b396eff7b9fbfd578f5965cb3e6ae3337
Jacob G. <jcgr@itu.dk>
2014-05-12 08:28:35 +0200

    Added endscreen just because

---
+++ CYOA/Assets/Scripts/EndGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour 
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") return;
+
+        Application.LoadLevel("EndScreen");
+    }
+}
---
+++ CYOA/Assets/Scripts/GameControl.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+	        Application.Quit();
+    }
+}
